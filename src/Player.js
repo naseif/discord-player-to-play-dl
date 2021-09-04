451dd164262555fb1c4e4ef7aa6f99d7a4a6ea87
naseif <d2mds21@gmail.com>
2021-09-04 16:10:09 +0000

    Added search function

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,16 @@
+const youtube = require("play-dl");
+
 class Player {
   constructor(client) {
     this.client = client;
   }
   on(event, callback) {}
+  async search(query, options) {
+    const searchForQuery = await youtube.search(query, options);
+    return {
+      tracks: searchForQuery,
+    };
+  }
 }
 
 module.exports = Player;
